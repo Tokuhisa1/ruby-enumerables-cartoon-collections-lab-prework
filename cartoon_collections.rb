f18d57b9b1f3b791ecdfda7d3b968770bdd45efd
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 21:09:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,9 @@ end
 
 def long_planeteer_calls(calls) # code an argument here
   # Your code here
+  calls.any do | call |
+    call.length
+  end
 end
 
 def find_the_cheese# code an argument here
