6991d7ecc3c9d15fe41533f28e36d4b8a5fe4323
Winston Tokuhisa <tokuhisawinston@outlook.com>
2021-03-30 21:12:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,4 +22,10 @@ end
 def find_the_cheese(foods) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  foods.each do | food |
+    if cheese_types.include(food)
+      return food
+    end
+  end
 end
